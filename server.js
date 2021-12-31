8899679fa2b89d31f8fdf72d73978b10454ce92f
neatHyperTxt <94314268+neatHyperTxt@users.noreply.github.com>
2021-12-31 22:39:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require('express');
-const { join } = require('path');
-const path = require('path');
 const app = express();
+const path = require('path');
 const bodyparser = require('body-parser');
 const session = require('express-session');
-const {v4:uuidv4} = require('uuid');
+// const {v4:uuid} = require('uuid');
 
-const router = require('./router');
-const port = process.env.PORT || 3000;
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:true}))
 app.set('view engine','ejs');
 
-app.use('/static',express.static(path.join(__dirname,'public')))
+app.use('/static',express.static(path.join(__dirname,'public')));
 app.use('/assets',express.static(path.join(__dirname,'public/assets')));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+// app.use(session(
+//     {
+//         secret:uuidv4(),
+//         resave:false,
+//         saveUninitialized:true
+// }))
 
-app.use(session(
-    {
-        secret:uuidv4(),
-        resave:false,
-        saveUninitialized:true
-    }))
-    app.use('/route',router); 
+app.listen(3000,()=>
+{
+    console.log('Listening to Port 3000');
+})
+
 app.get('/',(req,res)=>
 {
-    res.render('base',{title:'Login System'});
+    res.render('base',{title:"Login System"});
 })
-app.listen(port);
