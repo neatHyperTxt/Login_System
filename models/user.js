51792d7dd03a597e780c89eb84ee70e889ab4a61
neatHyperTxt <94314268+neatHyperTxt@users.noreply.github.com>
2022-02-22 22:45:20 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const {Schema,model} = mongoose;
 
+const userSchema = new Schema({
+    email:{
+        type:String,
+        required:[true,'Email is Required']
+    },
+    password:{
+        type:String,
+        require:[true,'Password is Required']
+    }
+})
+userSchema.statics.isValid = async function(email,password){
+    const foundUser = await this.findOne({email});
+    const validPassword = await bcrypt.compare(password,foundUser.password);
+    return (validPassword?foundUser:false);
+}
+userSchema.pre('save',async function(next)
+{
+    this.password = await bcrypt.hash(this.password,12)
+    next();
+})
+const User = model('User',userSchema);
+module.exports = User;
